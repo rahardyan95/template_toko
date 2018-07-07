727e95eafddf1af1294787598b43b4912152ae60
Rahardyan Putra <Rahardyan Putra>
2018-07-07 21:40:36 +0700

    pecahan

---
+++ pecahan/footer.php
@@ -0,0 +1,83 @@
+<!-- footer -->
+<div class="footer">
+	<div class="footer_agile_inner_info_w3l">
+		<div class="col-md-3 footer-left">
+			<h2><a href="index.html"><span>UG</span>Computer </a></h2>
+			<p>Toko yang menjual berbagai perlengkapan komputer dari berbagai jenis seperti gaming, multimedia, perkantoran, dll.</p>
+			<ul class="social-nav model-3d-0 footer-social w3_agile_social two">
+															<li><a href="#" class="facebook">
+																  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="twitter"> 
+																  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="instagram">
+																  <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="pinterest">
+																  <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
+														</ul>
+		</div>
+		<div class="col-md-9 footer-right">
+			<div class="sign-grds">
+				<div class="col-md-4 sign-gd">
+					<h4>Informasi <span>Web</span> </h4>
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="mens.html">Processor</a></li>
+						<li><a href="womens.html">VGA</a></li>
+						<li><a href="mens.html">RAM</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+				
+				<div class="col-md-5 sign-gd-two">
+					<h4>Informasi <span>Toko</span></h4>
+					<div class="w3-address">
+						<div class="w3-address-grid">
+							<div class="w3-address-left">
+								<i class="fa fa-phone" aria-hidden="true"></i>
+							</div>
+							<div class="w3-address-right">
+								<h6>Phone Number</h6>
+								<p>+0123456789</p>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+						<div class="w3-address-grid">
+							<div class="w3-address-left">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<div class="w3-address-right">
+								<h6>Email Address</h6>
+								<p>Email :<a href="mailto:example@email.com"> rahardyanputra95@gmail.com</a></p>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+						<div class="w3-address-grid">
+							<div class="w3-address-left">
+								<i class="fa fa-map-marker" aria-hidden="true"></i>
+							</div>
+							<div class="w3-address-right">
+								<h6>Location</h6>
+								<p>Mangga Dua Mall 
+								
+								</p>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+
+		<div class="clearfix"></div>
+	</div>
+		<p class="copy-right">&copy 2018 UG Computer. All rights reserved</p>
+	</div>
+</div>
+<!-- //footer -->
---
+++ pecahan/header.php
@@ -0,0 +1,238 @@
+<!-- header -->
+<div class="header" id="home">
+	<div class="container">
+		<ul>
+		    <li> <a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-unlock-alt" aria-hidden="true"></i> Sign In </a></li>
+			<li> <a href="#" data-toggle="modal" data-target="#myModal2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Sign Up </a></li>
+			<li><i class="fa fa-phone" aria-hidden="true"></i> Call : 01234567898</li>
+			<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:rahardyanputra95@gmail.com">rahardyanputra95@gmail.com</a></li>
+		</ul>
+	</div>
+</div>
+<!-- //header -->
+<!-- header-bot -->
+<div class="header-bot">
+	<div class="header-bot_inner_wthreeinfo_header_mid">
+		<div class="col-md-4 header-middle">
+			<form action="#" method="post">
+					<input type="search" name="search" placeholder="Search here..." required>
+					<input type="submit" value=" ">
+				<div class="clearfix"></div>
+			</form>
+		</div>
+		<!-- header-bot -->
+			<div class="col-md-4 logo_agile">
+				<h1><a href="index.html"><span>UG</span>Computer <!-- <i class="fa fa-shopping-bag top_logo_agile_bag" aria-hidden="true"></i> --></a></h1>
+			</div>
+        <!-- header-bot -->
+		<div class="col-md-4 agileits-social top_content">
+						<ul class="social-nav model-3d-0 footer-social w3_agile_social">
+						                                   <li class="share">Share On : </li>
+															<li><a href="#" class="facebook">
+																  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="twitter"> 
+																  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="instagram">
+																  <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
+															<li><a href="#" class="pinterest">
+																  <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+																  <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
+														</ul>
+
+
+
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!-- //header-bot -->
+<!-- banner -->
+<div class="ban-top">
+	<div class="container">
+		<div class="top_nav_left">
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse menu--shylock" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav menu__list">
+					<li class="active menu__item menu__item--current"><a class="menu__link" href="index.html">Home <span class="sr-only">(current)</span></a></li>
+					<li class="dropdown menu__item">
+						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Processor <span class="caret"></span></a>
+							<ul class="dropdown-menu multi-column columns-3">
+								<div class="agile_inner_drop_nav_info">
+									<div class="col-sm-6 multi-gd-img1 multi-gd-text ">
+										<a href="mens.html"><img src="images/top2.jpg" alt=" "/></a>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="processor.html">Intel Socket LGA 2066</a></li>
+											<li><a href="processor.html">Intel Socket LGA 1151</a></li>
+											<li><a href="processor.html">Intel Socket LGA 1150</a></li>
+											<li><a href="processor.html">Intel Socket LGA 2011</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="processor.html">AMD Socket TR4</a></li>
+											<li><a href="processor.html">AMD Socket AM4</a></li>
+										</ul>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</ul>
+					</li>
+					<li class="dropdown menu__item">
+						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">VGA <span class="caret"></span></a>
+							<ul class="dropdown-menu multi-column columns-3">
+								<div class="agile_inner_drop_nav_info">
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="womens.html">GALAX Geforce GTX 1080 Ti 11GB DDR5X 352 Bit HOF (HALL OF FAME) - With LCD Display</a></li>
+											<li><a href="womens.html">GALAX GeForce GTX 1070 Ti 8GB DDR5 HOF (HALL OF FAME) - Triple Fan</a></li>
+											<li><a href="womens.html">GALAX GeForce GTX 1070 8GB DDR5 HOF (HALL OF FAME) - Triple Fan</a></li>
+											<li><a href="womens.html">Asus Geforce GTX 1080 Ti 11GB DDR5X - ROG Poseidon</a></li>
+											<li><a href="womens.html">Asus Geforce GTX 1080 Ti 11GB DDR5X - Strix OC</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="womens.html">XFX Radeon RX VEGA 64 8GB HBM2 - Liquid Edition</a></li>
+											<li><a href="womens.html">XFX Radeon RX VEGA 56 8GB HBM2 </a></li>
+											<li><a href="womens.html">PowerColor Radeon RX 580 8GB DDR5 Red Devil Golden Edition</a></li>
+											<li><a href="womens.html">PowerColor Radeon RX 570 4GB DDR5 Red Devil</a></li>
+											<li><a href="womens.html">HIS Radeon RX 560 iCooler OC 4GB DDR5</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-6 multi-gd-img multi-gd-text ">
+										<a href="womens.html"><img src="images/top1.jpg" alt=" "/></a>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</ul>
+					</li>
+					<li class="dropdown menu__item">
+						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">RAM <span class="caret"></span></a>
+							<ul class="dropdown-menu multi-column columns-3">
+								<div class="agile_inner_drop_nav_info">
+									<div class="col-sm-6 multi-gd-img1 multi-gd-text ">
+										<a href="mens.html"><img src="images/top3.jpg" alt=" "/></a>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="mens.html">Corsair DDR4 Value 4GB PC19200 - CMV4GX4M1A2400C16 (1 X 4GB)</a></li>
+											<li><a href="mens.html">Corsair DDR4 Value 8GB PC19200 - CMV8GX4M1A2400C16 (1 X 8GB)</a></li>
+											<li><a href="mens.html">Corsair DDR4 Value 16GB PC19200 - CMV16GX4M1A2400C16 (1 X 16GB)</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="mens.html">Avexir DDR4 Core Red PC19200 4GB (1x4GB) Single Channel</a></li>
+											<li><a href="mens.html">Avexir DDR4 Core Blue PC19200 8GB (1x8GB) Single Channel</a></li>
+											<li><a href="mens.html">Avexir DDR4 Core White PC19200 16GB (1x16GB)</a></li>
+										</ul>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</ul>
+					</li>
+					<li class=" menu__item"><a class="menu__link" href="about.html">About</a></li>
+					<li class=" menu__item"><a class="menu__link" href="contact.html">Contact</a></li>
+				  </ul>
+				</div>
+			  </div>
+			</nav>	
+		</div>
+		<div class="top_nav_right">
+			<div class="wthreecartaits wthreecartaits2 cart cart box_1"> 
+						<form action="#" method="post" class="last"> 
+						<input type="hidden" name="cmd" value="_cart">
+						<input type="hidden" name="display" value="1">
+						<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
+					</form>  
+  
+						</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!-- //banner-top -->
+<!-- banner -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="2" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="3" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="4" class=""></li> 
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Processor <span>Collection</span></h3>
+						<p>Special untuk hari ini Intel Gen8</p>
+						<a class="hvr-outline-out button2" href="mens.html">Shop Now </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item2"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Motherboard <span>Collection</span></h3>
+						<p>New Arrivals On Sale ROG MAXIMUS VII</p>
+						<a class="hvr-outline-out button2" href="mens.html">Shop Now </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item3"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>RAM <span>Collection</span></h3>
+						<p>CORSAIR DOMINATOR EXTREME</p>
+						<a class="hvr-outline-out button2" href="mens.html">Shop Now </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item4"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>VGA <span>Collection</span></h3>
+						<p>Hadir GTX1080 ROG STRIX</p>
+						<a class="hvr-outline-out button2" href="mens.html">Shop Now </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item5"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Case <span>Thermaltake</span></h3>
+						<p>Thermaltake Level 20 Available!!</p>
+						<a class="hvr-outline-out button2" href="mens.html">Shop Now </a>
+					</div>
+				</div>
+			</div> 
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		<!-- The Modal -->
+    </div> 
+	<!-- //banner -->
